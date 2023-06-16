49f5588f9bac806629ed2e9a2702c96ec234277e
RezaRfahi <refahireza82@gmail.com>
2023-06-16 13:30:28 +0430

    Create Project with Identity

---
+++ ShopMVC/ShopMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
