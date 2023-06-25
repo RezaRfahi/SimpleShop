a3eb616b4931ee939d189250e448766cf98ed450
RezaRfahi <refahireza82@gmail.com>
2023-06-25 19:38:37 +0330

    CartItemController was Added

---
+++ ShopMVC/ShopMVC/Controllers/CartItemController.cs
@@ -0,0 +1,145 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopMVC.Data;
+using ShopMVC.Models;
+
+namespace ShopMVC.Controllers
+{
+    public class CartItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CartItem
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.CartItem.ToListAsync());
+        }
+
+        // GET: CartItem/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItem
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(cartItem);
+        }
+
+        // GET: CartItem/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CartItem/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Quantity,Price")] CartItem cartItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cartItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cartItem);
+        }
+
+        // GET: CartItem/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItem.FindAsync(id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+            return View(cartItem);
+        }
+
+        // POST: CartItem/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Quantity,Price")] CartItem cartItem)
+        {
+            if (id != cartItem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cartItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartItemExists(cartItem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cartItem);
+        }
+
+        // GET: CartItem/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cartItem = await _context.CartItem
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(cartItem);
+        }
+
+        // POST: CartItem/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cartItem = await _context.CartItem.FindAsync(id);
+            _context.CartItem.Remove(cartItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartItemExists(int id)
+        {
+            return _context.CartItem.Any(e => e.Id == id);
+        }
+    }
+}
