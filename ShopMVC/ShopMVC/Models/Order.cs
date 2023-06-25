381a5e05543118dba86f0a146cd28d99864d60a7
RezaRfahi <refahireza82@gmail.com>
2023-06-25 20:25:34 +0330

    Order Model Was added

---
+++ ShopMVC/ShopMVC/Models/Order.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopMVC.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+    
+        [Required]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    
+        [Display(Name = "Order Date")]
+        public DateTime OrderDate { get; set; }
+    
+        [Display(Name = "Total Amount")]
+        [DataType(DataType.Currency)]
+        [Range(0, double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
+        public decimal TotalAmount { get; set; }
+    
+        // Other properties as per your requirements
+    
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
